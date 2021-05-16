fd4f0c3a20137316c93b6425972912f27842e701
tadotcom <kh.sk.ti.1820@gmail.com>
2021-05-17 00:40:14 +0900

    リサイクルビューの挙動を設定するCustomAdapterを追加

--- app/src/main/java/com/example/todolist/CustomAdapter.kt
+++ app/src/main/java/com/example/todolist/CustomAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.todolist
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomAdapter {
+class CustomAdapter (private val rowDataList: ArrayList<RowData>): RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
 
     // Viewの初期化
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -20,4 +23,21 @@ class CustomAdapter {
             transferDetail = view.findViewById(R.id.transferDetail)
         }
     }
+
+    // レイアウトの設定
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.list_item, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    // Viewの設定
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val animal = rowDataList[position]
+
+        viewHolder.title.text = animal.title
+        viewHolder.detail.text = animal.detail
+    }
+
+    // 表示数を返す
+    override fun getItemCount() = rowDataList.size
 }
