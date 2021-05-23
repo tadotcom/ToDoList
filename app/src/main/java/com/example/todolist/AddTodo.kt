0058bcbbd3c0d695ebc55307025feea78736b7c5
tadotcom <kh.sk.ti.1820@gmail.com>
2021-05-23 23:31:59 +0900

    マニフェストファイルにAddTodoを追加

--- app/src/main/java/com/example/todolist/AddTodo.kt
+++ app/src/main/java/com/example/todolist/AddTodo.kt
@@ -1,4 +1,18 @@
 package com.example.todolist
 
-class AddTodo {
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class AddTodo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_todo)
+
+        val titleEdit: EditText = findViewById(R.id.titleEdit)
+        val detailEdit: EditText = findViewById(R.id.detailEdit)
+
+
+    }
 }
