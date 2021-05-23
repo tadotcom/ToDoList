e29c084834ff53895af52f2e177a7cfa7c97581a
tadotcom <kh.sk.ti.1820@gmail.com>
2021-05-24 00:51:15 +0900

    メイン画面の追加ボタンの追加、追加ボタンを押したときの挙動を実装

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -30,5 +33,29 @@ class MainActivity : AppCompatActivity() {
         // CustomAdapterの生成と設定
         mAdapter = CustomAdapter(mRowdate)
         recyclerView.adapter = mAdapter
+    }
+
+    /**
+     * ToolBarのメニューを初期化するためのメソッド
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.toolbar_option_buttons, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    /**
+     * ToolBarのメニューのクリックイベントを初期化するためのメソッド
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.add -> {
+                val intent = Intent(this, AddTodo::class.java)
+                intent.putExtra("VALUE", "")
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
