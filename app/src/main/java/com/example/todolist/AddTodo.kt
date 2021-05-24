405d41c9c90d8a1ac5ae37d938e633f1252d592d
tadotcom <kh.sk.ti.1820@gmail.com>
2021-05-24 23:38:21 +0900

    タスク追加ページ下部ボタン押下時の挙動を設定

--- app/src/main/java/com/example/todolist/AddTodo.kt
+++ app/src/main/java/com/example/todolist/AddTodo.kt
@@ -1,6 +1,8 @@
 package com.example.todolist
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,6 +15,20 @@ class AddTodo : AppCompatActivity() {
         val titleEdit: EditText = findViewById(R.id.titleEdit)
         val detailEdit: EditText = findViewById(R.id.detailEdit)
 
+        //キャンセルボタン押下
+        val cancelButton: Button = findViewById(R.id.cancelBtn)
+        cancelButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("VALUE", "")
+            startActivity(intent)
+        }
 
+        //リストに登録するボタンを押下
+        val registerButton: Button = findViewById(R.id.registerBtn)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("VALUE", "")
+            startActivity(intent)
+        }
     }
 }
