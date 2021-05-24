6e4a47ab3075f1d4ee0e0ef880732b3fb63723b1
tadotcom <kh.sk.ti.1820@gmail.com>
2021-05-25 00:03:57 +0900

    タスク詳細画面下部ボタンの挙動を設定

--- app/src/main/java/com/example/todolist/DetailTodo.kt
+++ app/src/main/java/com/example/todolist/DetailTodo.kt
@@ -1,6 +1,8 @@
 package com.example.todolist
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,5 +14,21 @@ class DetailTodo : AppCompatActivity() {
 
         val titleTextView: TextView = findViewById(R.id.title)
         val detailTextView: TextView = findViewById(R.id.detail)
+
+        //キャンセルボタン押下
+        val cancelButton: Button = findViewById(R.id.cancelBtn)
+        cancelButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("VALUE", "")
+            startActivity(intent)
+        }
+
+        //削除ボタン押下
+        val deleteButton: Button = findViewById(R.id.deleteBtn)
+        deleteButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("VALUE", "")
+            startActivity(intent)
+        }
     }
 }
